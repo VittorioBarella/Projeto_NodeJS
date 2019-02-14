3f5bf34e5d292fa3246099a8decf3e6627ddd4d6
Andrade <andradv@ad.esi.adp.com>
2019-02-14 11:42:54 -0200

    Organização do projeto

--- server.js
+++ server.js
@@ -3,15 +3,35 @@
 const http = require('http');
 
 const servidor  = http.createServer(function (req, resp) {
-    resp.end(`
+
+    let html = '';
+
+    if(req.url == '/') {
+
+        html = `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Testando Servidor </h1>
+                </body>
+            </html>
+        `;
+    } else if (req.url == '/testeServ2') {
+
+        html = `
         <html>
             <head>
                 <meta charset="utf-8">
             </head>
             <body>
-                <h1> Testando Servidor </h1>
+                <h1> Testando Servidor 2 </h1>
             </body>
         </html>
-    `);
+    `;
+
+    }
+    resp.end(html);
 });
 servidor.listen(3000);
