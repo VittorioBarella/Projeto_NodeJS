e669201f0ae953add0b0867b77612fd642d8fb24
Andrade <andradv@ad.esi.adp.com>
2019-02-27 11:33:07 -0300

    Atualização dos arquivos.

--- server.js
+++ server.js
@@ -1,43 +1,34 @@
-// CRIAÇÃO DE UM EXEMPLO DE SERVIDOR WEB.
-const express = require('express');
-const app = express();
+const http = require('http');
 
-app.listen(3000, function(){
-    console.log('Servidor na porta 3000.');
+const servidor = http.createServer(function (req, resp) {
+
+    let html = '';
+    if (req.url == '/') {
+        html = `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Casa do Código </h1>
+                </body>
+            </html>
+        `;
+    } else if (req.url == '/livros') {
+        html = `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Listagem de livros </h1>
+                </body>
+            </html>
+        `;
+    }
+
+    // outros else-if
+
+    resp.end(html);
 });
-
-// const http = require('http');
-
-// const servidor  = http.createServer(function (req, resp) {
-
-//     let html = '';
-
-//     if(req.url == '/') {
-
-//         html = `
-//             <html>
-//                 <head>
-//                     <meta charset="utf-8">
-//                 </head>
-//                 <body>
-//                     <h1> Testando Servidor </h1>
-//                 </body>
-//             </html>
-//         `;
-//     } else if (req.url == '/testeServ2') {
-
-//         html = `
-//         <html>
-//             <head>
-//                 <meta charset="utf-8">
-//             </head>
-//             <body>
-//                 <h1> Testando Servidor 2 </h1>
-//             </body>
-//         </html>
-//     `;
-
-//     }
-//     resp.end(html);
-// });
-// servidor.listen(3000);
+servidor.listen(3000);
